62c5140dbc50e2d83ffdc5abaac08d932750dcc4
zenblade3383 <48926193+zenblade3383@users.noreply.github.com>
2019-08-21 07:55:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const varFind = function(predicate, array, object) {
+const varFind = function(predicate, object) {
+	const isElement = function (element) {
+    		return element instanceof Element || element instanceof HTMLDocument;  
+	}
 	const varSearch = function(obj, predicate) { // The function 
 		const discoveredObjects = []; // For checking for cyclic object
 		const path = []; // The current path being searched
@@ -10,24 +13,29 @@ const varFind = function(predicate, array, object) {
 		if (typeof predicate !== "function") {
 			throw new TypeError("Predicate is not a function");
 		}
-		(function find(obj) {
+		const find = function (obj, cyclicDetect) {
 			for (const key of Object.keys(obj)) { // use only enumrable own properties.
 				if (predicate(key, path, obj) === true) { // Found a path
 					path.push('["' + key + '"]'); // push the key
 					results.push(path.join("")); // Add the found path to results
 					path.pop(); // remove the key.
 				}
+				var newCyclicDetect = [];
+				if(cyclicDetect && typeof(cyclicDetect) === "object"){
+					newCyclicDetect = cyclicDetect;
+				}
 				const o = obj[key]; // The next object to be searched
-				if (o && typeof o === "object") { // check for null then type object
+				if (o && typeof o === "object" && !isElement(o)) { // check for null then type object
 					if (!discoveredObjects.find(obj => obj === o)) { // check for cyclic link
 						path.push('["' + key + '"]');
 						discoveredObjects.push(o);
-						find(o);
+						find(o, newCyclicDetect.push(o));
 						path.pop();
 					}
 				}
 			}
-		}(obj));
+		};
+		find(obj);
 		for (var i in results) {
 			results[i] = "window" + results[i]
 		}
@@ -46,14 +54,14 @@ const varFind = function(predicate, array, object) {
 	};
 	const paths = [
 		[],
-		array
+		object.array
 	];
-	if (typeof(object) === "undefined") {
+	if (typeof(object.object) === "undefined") {
 		paths[0] = varSearch(window, predicate);
 	} else {
-		paths[0] = varSearch(object, predicate);
+		paths[0] = varSearch(object.object, predicate);
 	}
-	if (typeof(array) === "undefined") {
+	if (typeof(object.array) === "undefined") {
 		return paths[0];
 	} else {
 		return similarities(paths[0], paths[1]);
