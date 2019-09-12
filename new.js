851587a443ff786c26b3178e52f7a3c153094138
zenblade3383 <48926193+zenblade3383@users.noreply.github.com>
2019-09-12 09:05:12 -0400

    Create new.js

---
+++ new.js
@@ -0,0 +1,41 @@
+const varFind = function (predicate, object) {
+	const results = [];
+	const isElement = function (element) {
+		return element instanceof Element || element instanceof HTMLDocument;  
+	}
+	const varSearch = function (obj, path, cyclicDetect) {
+		if(typeof(path) == "undefined") {
+			path = [
+				"window"
+			];
+			cyclicDetect = [];
+		}
+		for (var key of obj) {
+			path.push(key);
+			if(predicate(obj,  key, path)) {
+				var editedPath = path
+				for (var i in path) {
+					if (i != 0) {
+						editedPath[i] = "['" + editedPath[i] + "']";
+					}
+				}
+				results.push(editedPath.join(""));
+				path.pop();
+			}
+			const o = obj[key];
+			var isCyclic = false;
+			if (o && typeof o === "object" && !isElement(o)) {
+				for(var i in cyclicDetect) {
+					if(cyclicDetect[i] == o) {
+						isCyclic = true;
+					}
+				}
+				if (!isCyclic) {
+					cyclicDetect.push(obj);
+					varSearch(o, path, cyclicDetect);
+				}
+			}
+		}
+	}
+	return results;
+}
