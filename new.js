c1c40733050a36eca553e0f2795ce773c97ecf93
zenblade3383 <48926193+zenblade3383@users.noreply.github.com>
2020-01-29 14:29:01 -0500

    Update new.js

--- new.js
+++ new.js
@@ -37,49 +37,49 @@ var varFind = function (predicate, object, layers) {
 		}
 		//looping through object
 		for (var key of Object.keys(obj)) {
-			//to prevent errors
-			try {
-				//layers is used more in lag reduction
-				layers = typeof(layers) == "number" ? layers : Infinity;
-				//pushing current key to path
-				path.push(key);
-				//if matches predicate
-				if(predicate(key, obj, path) === true) {
-					//cloning path for modification
-					var editedPath = [...path];
-					//putting in propper formatt
-					for (var i in path) {
-						if (i != 0) {
-							editedPath[i] = "['" + editedPath[i].replace(/'/g, "\\'") + "']";
-						}
-					}
-					//pushing result to results
-					results.push(editedPath.join(""));
-				}
-				//to help with cyclic detection
-				var isCyclic = false;
-				//for easier access
-				const o = obj[key];
-				//testing if element to prevent cors error and if it is an object
-				if (o && typeof o === "object" && !isElement(o) && layers != 0) {
-					//pushing object to cyclic detect
-					cyclicDetect.push(obj);
-					for(var i in cyclicDetect) {
-						//checking if cyclic
-						if(cyclicDetect[i] == o) {
-							isCyclic = true;
-						}
-					}
-					//recursion!
-					if (!isCyclic) {
-						varSearch(o, path, cyclicDetect, layers - 1);
-					}
-					//popping cyclicDetect for future objects not in that recursion
-					cyclicDetect.pop(obj);
-				}
-				//popping path for the next possible path
-				path.pop();
-			} catch (e) {}
+            //layers is used more in lag reduction
+            layers = typeof(layers) == "number" ? layers : Infinity;
+            //pushing current key to path
+            path.push(key);
+            //if matches predicate
+            try {
+                if(predicate(key, obj, path)) {
+                    //cloning path for modification
+                    var editedPath = [...path];
+                    //putting in propper formatt
+                    for (var i in path) {
+                        if (i != 0) {
+                            editedPath[i] = "['" + editedPath[i].replace(/'/g, "\\'") + "']";
+                        }
+                    }
+                    //pushing result to results
+                    results.push(editedPath.join(""));
+                }
+            } catch (e) {}
+            //to help with cyclic detection
+            var isCyclic = false;
+            //for easier access
+            var o;
+            try{o = obj[key];}catch(e){}
+            //testing if element to prevent cors error and if it is an object
+            if (o && typeof o === "object" && !isElement(o) && layers != 0) {
+                //pushing object to cyclic detect
+                cyclicDetect.push(obj);
+                for(var i in cyclicDetect) {
+                    //checking if cyclic
+                    if(cyclicDetect[i] == o) {
+                        isCyclic = true;
+                    }
+                }
+                //recursion!
+                if (!isCyclic) {
+                    varSearch(o, path, cyclicDetect, layers - 1);
+                }
+                //popping cyclicDetect for future objects not in that recursion
+                cyclicDetect.pop(obj);
+            }
+            //popping path for the next possible path
+            path.pop();
 		}
 	}
 	//getting results
